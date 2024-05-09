f41f71191ba1a75220a30176261a45dea148dbb7
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-05-09 17:37:21 -0400

    created the relationships between models and set up posts and comments factories

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,13 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
  */
-class PostFactory extends Factory
+class CommentFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -19,8 +20,8 @@ class PostFactory extends Factory
     {
         return [
             'user_id' => User::factory(),
-            'title' => str(fake()->sentence())->beforeLast('.')->title(),
-            'body' => fake()->realText(600),
+            'post_id' => Post::factory(),
+            'body' => fake()->realText(250),
         ];
     }
 }
